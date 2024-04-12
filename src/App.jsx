8803634865757bc5bf6fb138f55640ad92a53230
Sayda <sayda@farletza.com.ec>
2024-04-12 18:34:10 -0500

    Practica Lista Reactjs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Catalogo from './componentes/Catalogo'
+
+function App() {
+
+  return (
+    <>
+    <Catalogo/>
+    </>
+  )
+}
+
+export default App
